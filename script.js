0c5ac8ca4261db117c16abfc706a50a46048d7d0
A.Adefolalu B <adefolalucode101@gmail.com>
2022-05-14 14:17:36 +0100

    create more functions to console and document

--- script.js
+++ script.js
@@ -1,9 +1,24 @@
-function sayHi() {
-    alert ("Hello world from Acedu !!!");
+// function sayHi() {
+//     alert ("Hello world from Acedu !!!");
 
+// }
+// sayHi();
+// sayHi();
+// sayHi();
+// sayHi();
+// sayHi();
+
+function saySomething(whatTosay) {
+    alert (whatTosay);
 }
-sayHi();
-sayHi();
-sayHi();
-sayHi();
-sayHi();
+saySomething("Hello world !!!")
+saySomething("How are you ?")
+saySomething("Are you a Developer ?")
+
+function add(number1, number2){ 
+    return number1 + number2;
+}
+alert(add(3,5));
+console.log(add(3,10));
+document.write(add(4,34));
+
